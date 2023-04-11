2d7938f216d66b11f078077604e58ee717035c48
SungGwon Jeon <jeonsg9904@gmail.com>
2023-04-11 21:34:39 +0900

    commit

--- src/main/java/com/example/Capstone/security/repository/UserRepository.java
+++ src/main/java/com/example/Capstone/security/repository/UserRepository.java
@@ -15,7 +15,6 @@ import java.util.Optional;
  */
 
 // JpaRepository 를 상속하면 자동 컴포넌트 스캔됨.
-@Qualifier("security_db")
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer>{
 	// SELECT * FROM user WHERE username = ?1
