8e9f833609c66af2c7a2273ee55b43313a326d4f
jeonseong-gwon <wjs4518@naver.com>
2023-08-23 19:20:56 +0900

    commit

--- src/main/java/com/example/Capstone/api/MessageController.java
+++ src/main/java/com/example/Capstone/api/MessageController.java
@@ -20,7 +20,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/messages")
-@Api(tags = "메시지")
+@Api(tags = "단일 메시지")
 @RequiredArgsConstructor
 public class MessageController {
     private final MessageService messageService;
